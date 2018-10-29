0f8644e0f198439ef09d17bbe04278c094625deb
FlorinnB <florinx90@gmail.com>
2018-10-29 16:01:36 +0200

    orie

--- html5/orientation.js
+++ html5/orientation.js
@@ -11,7 +11,7 @@ function on_device_orientation(e)
 	
 	var canvas=document.getElementById("id_canvas");
 	var context=canvas.getContext("2d");
-	context.clearRect(0,0,canvas.width,canvas,height);
+	context.clearRect(0,0,canvas.width,canvas.height);
 	var R=10;
 	context.beginPath();
 	context.arc(canvas.width/2 + e.gamma/90 * (canvas.width/2 -R),canvas.height+e.beta/90*(canvas.height/2-R),R,0,2*Math.Pi);
