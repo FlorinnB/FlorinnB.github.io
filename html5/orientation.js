e14a7ab1cf66bbfa6922eda461c14e2996a3d685
FlorinnB <florinx90@gmail.com>
2018-11-05 13:22:50 +0200

    acc

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,6 +1,6 @@
 
 window.addEventListener("deviceorientation",on_device_orientation);
-
+window.addEventListener("devicemotion",on_device_motion);
 
 
 function on_device_orientation(e)
@@ -22,4 +22,12 @@ function on_device_orientation(e)
 	
 	
 	
+}
+
+
+function on_device_motion(e)
+{
+	document.getElementById("id_acc_z").innerHTML=e.accelerationIncludingGravity.z;
+	document.getElementById("id_acc_x").innerHTML=e.accelerationIncludingGravity.x ;
+	document.getElementById("id_acc_y").innerHTML=e.accelerationIncludingGravity.y;
 }
